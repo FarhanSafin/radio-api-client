ef72e80a95a24cd566a2470aa763bb8deb8dce2e
Farhan <farhansafin@gmail.com>
2022-06-04 12:27:29 +0600

    data fetched and shown

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import useRadios from '../../hooks/useRadios';
 
 const Home = () => {
+    const [radios] = useRadios();
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='container mx-auto text-center'>
+            <h2 className='mt-20'>Available Radios: {radios.length}</h2>
+            <div className="overflow-x-auto">
+            <table className="table w-full mt-52">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Radio Station's Name</th>
+        <th>Radio Station's Frequency</th>
+      </tr>
+    </thead>
+    <tbody>
+    {
+        radios.map((radio, index)=>      <tr key={radio._id}>
+        <th>{index+1}</th>
+        <td>{radio.name}</td>
+        <td>{radio.frequency}</td>
+      </tr>)
+    }
+    </tbody>
+  </table>
+  </div>
         </div>
     );
 };
---
+++ src/hooks/useRadios.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+
+const useRadios = () => {
+
+    const [radios, setRadios] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/radionames')
+        .then(res => res.json())
+        .then(data => setRadios(data));
+    }, []);
+
+
+    return [radios, setRadios];
+
+    }
+
+export default useRadios;
