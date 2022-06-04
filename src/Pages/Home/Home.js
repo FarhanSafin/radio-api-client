c55edb4d5d041263aed6668c32a8b5fcad6a7c2c
Farhan <farhansafin@gmail.com>
2022-06-04 12:49:43 +0600

    Radio add api, form added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import useRadios from '../../hooks/useRadios';
+import Loading from '../Shared/Loading/Loading'
 
 const Home = () => {
     const [radios] = useRadios();
-    return (
+
+
+    if(radios.length === 0){
+      return <Loading></Loading>
+    }else{
+      return (
         <div className='container mx-auto text-center'>
             <h2 className='mt-20'>Available Radios: {radios.length}</h2>
             <div className="overflow-x-auto">
@@ -28,6 +34,11 @@ const Home = () => {
   </div>
         </div>
     );
+    }
+
+
+
+    
 };
 
 export default Home;
